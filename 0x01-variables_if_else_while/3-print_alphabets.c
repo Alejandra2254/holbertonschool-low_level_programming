d6fe07483c02e0811f326c6a5a9c435ee0f29d0e
alejandra <alejandra.hmsc@gmail.com>
2020-09-17 16:45:53 -0500

    alfabeto

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int alp
+for (alp= 'a'; alp<= 'z'; ch++)
+putchar("alp")
+putchar("\n")
+return (0);
+}
