7b950ad630b774f705ed96996862bd2bab4c814b
alejandra <alejandra.hmsc@gmail.com>
2020-09-17 23:45:27 -0500

    o

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int nu;
+for (nu = '0'; nu <= '9'; nu++)
+{
+putchar(nu);
+}
+putchar('\n');
+return (0);
+}
