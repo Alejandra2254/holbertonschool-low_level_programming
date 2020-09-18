a0f486df217787a806f5f684144ce76bbced2890
alejandra <alejandra.hmsc@gmail.com>
2020-09-18 12:09:50 -0500

    p

---
+++ 0x01-variables_if_else_while/10-print-comb2.c
@@ -0,0 +1,28 @@
+#incluide <stdio.h>
+/**
+* main - Entry point 
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int nm;
+for (nm = 0; nm < 100 ; m++)
+{
+putchar((nm / 10) + '0');
+putchar((nm % 10) + '0');
+if (nm != 99)
+{
+putchar(',');
+putchar(' ');
+}
+else
+{
+putchar('\n');
+}
+}
+return (0);
+}
+
+	
+      
