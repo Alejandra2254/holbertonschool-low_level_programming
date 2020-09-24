5074f5a1158159b6d31190278f15e8f9a01c354d
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-09-24 10:01:03 -0500

    l

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul -  checks for checks for a digit (0 through 9).
+ * @a: first integer
+ * @b: second integer
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
