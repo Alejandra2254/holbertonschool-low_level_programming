3d15006a7d41ad75412e9171ccf6f50f5a9bde0d
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-09-24 23:32:04 -0500

    p

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_triangle -  checks for checks for a digit (0 through 9).
+ * @size: size -  Variable
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if (j <= size - 1)
+					_putchar(' ');
+				else
+					_putchar('#');
+				if (j == size)
+					_putchar('\n');
+			}
+		}
+	}
+	else
+		_putchar('\n');
+}
