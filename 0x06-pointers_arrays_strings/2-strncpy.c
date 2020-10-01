953586d588ad89b34e6b569c37d7de151c0e3426
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-10-01 12:55:49 -0500

    p

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,19 +1,22 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * _strncpy - copy string
+ * @dest: where
+ * @src: from
+ * @n: number of characters
+ * Return: dest funtion where was copy
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
-	for (i = 0; src[i] != '\0'; i++)
+	int i = 0;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
 	for ( ; i < n; i++)
 		dest[i] = '\0';
-	return (src);
+	return (dest);
 }
