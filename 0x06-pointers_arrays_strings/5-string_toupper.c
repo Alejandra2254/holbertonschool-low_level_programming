34a67f80865dd796763aaf5c4de8983c96a75a9b
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-10-01 16:09:07 -0500

    p

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - changes all lowecase letters
+ * @d: string
+ * return: uppercase string
+ */
+char *string_toupper(char *d)
+{
+	int i = 0, j;
+
+	while (d[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	for (j = 0; j <= i; j++)
+	{
+		if (d[j] >= 97 && d[j] <= 122)
+		{
+			d[j] = d[j] - 32;
+		}
+	}
+	return (d);
+}
