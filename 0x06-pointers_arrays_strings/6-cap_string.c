6519354bd63ca240dea6b34f957637f6f9cb1e21
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-10-01 16:35:06 -0500

    p

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capital letters.
+ * @s: s is the array
+ * Return: Always 0.
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == 32 || s[i] == '\n' || s[i] == 44 || s[i] == 59
+		    || s[i] == 46 || s[i] == 33 || s[i] == 63 || s[i] == 34
+		    || s[i] == 40 || s[i] == 123 || s[i] == 125 )
+		{
+			if (s[i + 1] <= 122 && s[i + 1] >= 97)
+				{
+					s[i + 1] = s[i + 1] - 32;
+				}
+		}
+		i++;
+	}
+	return (s);
+}
