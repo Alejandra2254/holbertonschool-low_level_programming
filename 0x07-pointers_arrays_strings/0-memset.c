f45c4840ff3b1bc93f2d1af1140934c21047adc1
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-10-05 12:49:29 -0500

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,20 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * _memset - fills memory with a constant byte
+ * @s: string
+ * @b: constant byte
+ * @n: size
+ * Return: memset string
  */
 char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return(s);
+}
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,7 +1,6 @@
-
 #ifndef _SAMPLE_HEADER_H_
 #define _SAMPLE_HEADER_H_
 #include <stdio.h>
 int _putchar (char c);
-
+char *_memset(char *s, char b, unsigned int n);
 #endif
