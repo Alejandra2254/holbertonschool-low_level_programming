f95c07762a1262c26955677cd4397a07a4ab3d29
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-10-11 20:00:57 -0500

    add

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- * main - prints
- * @argc: number of comand line arguments
- * @argv: An array
- * Return: 0
+ * main - prints buffer in hexa
+ * @argc: he number of command line arguments
+ * @argv: An array containing the program command line arguments.
+ *
+ * Return: Nothing.
  */
-
 int main(int argc, char *argv[])
 {
-	(void)argv;
 	int count;
 
+	(void)argv;
 	for (count = 0; count < argc; count++)
 	{
 		if (count + 1 == argc)
