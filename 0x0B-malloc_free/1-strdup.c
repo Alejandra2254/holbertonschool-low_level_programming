e9d29258a013c79b2dc5aa8be7a83edf027fd5c1
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-10-13 12:42:50 -0500

    malloc 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+char *_strdup(char *str)
+{
+	char *s;
+	int i = 0;
+	int c;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i] !='\0')
+		i++;
+	s = malloc(i * sizeof(*s) + 1);
+
+	for (c = 0; c < i; c++)
+		s[c] = str[c];
+	s[i] = '\0';
+	return (s);
+}
