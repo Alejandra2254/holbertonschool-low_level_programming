9a462cf5f55f855bbb8e465ef2c94d26c6fc7521
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-10-13 22:11:28 -0500

    free

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * argstostr - concatenate all arguments
+ * @ac: argument count
+ * @av: double pointer to array of strings passed to main
+ * Return: NULL if fail, else return pointer to new string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+}
