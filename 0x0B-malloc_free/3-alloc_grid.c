429bd71626edbaf9005d4518c93950e40debbe6a
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-10-13 21:11:52 -0500

    update alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid -
- * @width:
- * @height:
+ * alloc_grid - pointer to a 2 dimensional array
+ * @width: width of array
+ * @height: height of array
  * Return: NULL on failure or width or height negative or 0
  */
 int **alloc_grid(int width, int height)
@@ -16,13 +16,13 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	a = malloc (height * sizeof(int *));
+	a = malloc(height * sizeof(int *));
 
 	if (a == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		a[i] = malloc (width * sizeof (int));
+		a[i] = malloc(width * sizeof(int));
 		if (a[i] == NULL)
 		{
 			return (NULL);
