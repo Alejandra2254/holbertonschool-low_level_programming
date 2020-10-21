3accd3206bfa4f735fbb31ec59f011da0fbf9bba
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-10-21 12:02:39 -0500

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - function to print name
+ * @name: character string
+ * @f: function pointer that takes character string arg
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
