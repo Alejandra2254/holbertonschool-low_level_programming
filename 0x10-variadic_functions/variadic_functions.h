26ad96061c6ba5b608ada90425f17148c9d344b4
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-10-24 15:25:00 -0500

    p

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,7 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 /**
  * struct form - Struct form
  *
@@ -15,9 +16,8 @@ void print_strings(const char *separator, const unsigned int n, ...);
 typedef struct form
 {
 	char *type;
-	void (*f)();
+	void (*f)(va_list arg);
 } form_;
-void print_all(const char * const format, ...);
 void _char(va_list arg);
 void _integer(va_list arg);
 void _float(va_list arg);
