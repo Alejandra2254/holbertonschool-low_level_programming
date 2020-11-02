41e74a09f0739129555414537fe633020d397568
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-11-02 11:24:57 -0500

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * __attribute__ ((constructor)) myConstructor - function to execute first
+ */
+void __attribute__ ((constructor)) myConstructor()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
