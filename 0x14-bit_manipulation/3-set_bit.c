698a14ca931aea02809e11a40388db6da616f1bc
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-11-05 14:50:00 -0500

    task 3 update

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * set_bit function that sets the value of a bit to 1
+ * set_bit - function that sets the value of a bit to 1
  * @n: number
  * @index: index
  * Return: 1 if it worked or -1 if an error ocurred
@@ -8,7 +8,7 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 
-	if (index > 63)
+	if (index > (sizeof(n) * 8))
 		return (-1);
 	*n = *n | 1 << index;
 	return (1);
