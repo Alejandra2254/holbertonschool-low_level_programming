73b9535bcb907da3be9ad156913500b354d40f95
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-11-05 12:46:49 -0500

    task 1, print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,14 +7,16 @@ void print_binary(unsigned long int n)
 {
 	int flag = 0;
 	int i, a = 1, b;
-	unsigned long int num = n;
-	unsigned long int p;
+      	unsigned long int p;
 
-	if (num == 0)
+	if (!n)
+	{
 		_putchar('0');
+		return;
+	}
 	for (i = 0; i < 32; i++)
 	{
-		p = ((a << (31 - i)) & num);
+		p = ((a << (31 - i)) & n);
 		if (p >> (31 - i))
 			flag = 1;
 		if (flag)
