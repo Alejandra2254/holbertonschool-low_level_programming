b6b0731d2258caf297466a8a67b33a79977c607f
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-11-09 12:09:36 -0500

    adding read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	s = malloc(letters * sizeof(char) + 1);
 	if (!s)
-		return(0);
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
