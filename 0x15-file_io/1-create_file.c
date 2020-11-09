4987a46ac9d5aca9ffbf79375a26c8d2a373a8ed
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-11-09 12:39:45 -0500

    fixing betty style

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 	op = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
-	if(op == -1)
+	if (op == -1)
 		return (-1);
 	while (text_content[i] != '\0')
 		i++;
