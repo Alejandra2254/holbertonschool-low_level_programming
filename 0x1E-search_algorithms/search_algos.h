4adbd516591ac2c3ea202e52cbebcf479b73ea06
Alejandra2254 <2254@holbertonschool.com>
2021-04-20 14:16:12 -0500

    Adding header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,11 @@
-#ifndef SEARCH_AlGOS
-#define SEARCH_ALGOS
-#include <stddef.h>
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
 #include <stdio.h>
+#include <stdlib.h>
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-#endif
+int jump_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
