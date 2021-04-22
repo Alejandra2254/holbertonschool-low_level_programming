d2f71cbdf0f7979d179833e655dd264b8c9c1136
Alejandra2254 <2254@holbertonschool.com>
2021-04-21 20:58:05 -0500

    adding intepolation

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+/**
+ * interpolation_search - searches for a value in a sorted array of integers
+ * using the Interpolation search algorithm
+ * @array: is a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: value is the value to search for
+ * Return: If value is not present in array or if
+ * array is NULL, your function must return -1
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	int low = 0, high = size - 1, mid;
+
+	while ((low <= high) && (mid < (high + 1)))
+	{
+		mid = low + ((value - array[low]) * (high - low) /
+			     (array[high] - array[low]));
+		if (value == array[mid])
+		{
+			printf("Value checked array[%d] = [%d]\n", mid, array[mid]);
+			return (mid);
+		}
+		else if (value < array[mid])
+		{
+			high = mid - 1;
+		}
+		else
+		{
+			low = mid + 1;
+		}
+	}
+	if (value == array[low])
+	{
+		return (low);
+	}
+	else
+	{
+		printf("Value checked array[%d] is out of range\n", mid);
+		return (-1);
+	}
+}
