0b8a0c5d2d1a32cc1af9f4d63c861d367a65c9e2
Alejandra2254 <2254@holbertonschool.com>
2021-04-21 21:10:06 -0500

    fixinf output of interpolation

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,6 +12,8 @@ int interpolation_search(int *array, size_t size, int value)
 {
 	int low = 0, high = size - 1, mid;
 
+	if (!array)
+		return (-1);
 	while ((low <= high) && (mid < (high + 1)))
 	{
 		mid = low + ((value - array[low]) * (high - low) /
